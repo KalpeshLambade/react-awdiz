617cb98289b1d9c7e99e421573baf1f654c28d30
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-18 11:14:21 +0530

    updated

--- awdiz/src/components/Effect.jsx
+++ awdiz/src/components/Effect.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 
 
 const Effect = () => {
-    const[number, setNumber] = useState(0);
-    const[number2, setNumber2] = useState(0);
+    // const[number, setNumber] = useState(0);
+    // const[number2, setNumber2] = useState(0);
+
+    const[products, setProducts] =useState();
 
     // useEffect(() => {alert("Working")});
     // will execute the code on every render
@@ -14,15 +16,30 @@ const Effect = () => {
     // useEffect(() => {alert("will execute code when state change")},[number]);
     //will execute the code very first time & the specific state will change
 
-    useEffect(() => {alert("for mulltiple state")}, [number,number2]);
+    // useEffect(() => {alert("for mulltiple state")}, [number,number2]);
     // will execute the code very first time & for multiple state that we passed
+
+    
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+        .then(res => res.json())
+        .then( Json => setProducts(Json));
+    });
 
     return(
         <div>
-            <h1>number {number}</h1>
+            {/* <h1>number {number}</h1>
             <h1> number 2 {number2}</h1>
             <button onClick={() => {setNumber(number+1)}}>Click</button>
-            <button onClick={() => {setNumber2(number2+1)}}>Click for 1</button>
+            <button onClick={() => {setNumber2(number2+1)}}>Click for 1</button> */}
+
+            {products && products.map((e) => (
+                <div>
+                    <div><img src={e.image}/></div>
+                    <h1>{e.title}</h1>
+                    <p>{e.price}</p>
+                </div>
+            ))}
         </div>
     );
 }
