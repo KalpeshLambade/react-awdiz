a97628f7a80baab2dc5fa8a6e99816e9f9d47e60
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-20 17:06:43 +0530

    updated

--- awdiz/src/App.jsx
+++ awdiz/src/App.jsx
@@ -2,7 +2,7 @@
 // import Footer from "./components/Footer";
 // import Range from "./components/Range";
 import Register from "./components/Register";
-// import Effect from "./components/Effect";
+import Effect from "./components/Effect";
 // import Efeect2 from "./components/Efeect2";
 import {Routes,Route} from 'react-router-dom';
 import Home from "./components/Home";
@@ -19,6 +19,7 @@ function App() {
         <Route exact path='/' element={<Home/>} />
         <Route exact path='/register' element={<Register/>} />
         <Route exact path='/login' element={<Login/>} />
+        <Route exact path="/effect" element={<Effect/>} />
       </Routes>
 
     </div>
