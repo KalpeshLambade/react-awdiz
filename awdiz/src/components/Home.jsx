a37b3ea5fa9b9d4173696f9aac9276fcf1af8b20
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-22 22:16:01 +0530

    updated

--- awdiz/src/components/Home.jsx
+++ awdiz/src/components/Home.jsx
@@ -1,8 +1,10 @@
+import { toast } from "react-hot-toast";
 
 function Home(){
     return(
         <div>
             <h1>Home</h1>
+            <button onClick={() => {toast.loading('Waiting...');}} >Toast</button>
         </div>
     )
 }
